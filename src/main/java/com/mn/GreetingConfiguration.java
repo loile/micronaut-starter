0e7e128f904a9a0af8e1b123fef9363bc397dadd
Loi Le <huyloile@gmail.com>
2021-02-26 07:15:37 +0100

    Add lombok dependency pom #3

--- src/main/java/com/mn/GreetingConfiguration.java
+++ src/main/java/com/mn/GreetingConfiguration.java
@@ -2,26 +2,22 @@ package com.mn;
 
 import io.micronaut.context.annotation.ConfigurationInject;
 import io.micronaut.context.annotation.ConfigurationProperties;
+import lombok.Getter;
 
 import javax.validation.constraints.NotBlank;
 
 @ConfigurationProperties(value = "hello.config.greeting")
 public class GreetingConfiguration {
 
+    @Getter
     private final String en;
+
+    @Getter
     private final String de;
 
     @ConfigurationInject
     public GreetingConfiguration(@NotBlank String en, @NotBlank String de) {
         this.en = en;
         this.de = de;
-    }
-
-    public String getDe() {
-        return this.de;
-    }
-
-    public String getEn() {
-        return this.en;
     }
 }
