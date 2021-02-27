416da2e1c848541f72a7710921976c208fa9b473
Loi Le <huyloile@gmail.com>
2021-02-27 07:33:30 +0100

    Configure OpenApi and Swagger #8

--- src/main/java/com/mn/Application.java
+++ src/main/java/com/mn/Application.java
@@ -2,9 +2,20 @@ package com.mn;
 
 import io.micronaut.context.ApplicationContext;
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "mv-stock-broker",
+                version = "0.1",
+                description = "Udemy Micronaut Service",
+                license = @License(name = "MIT")
+        )
+)
 public class Application {
 
     private static final Logger LOG = LoggerFactory.getLogger(Application.class);
