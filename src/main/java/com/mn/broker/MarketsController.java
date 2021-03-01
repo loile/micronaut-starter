9f8f2a90e1a7646736f30e9c5c1545b24b610fdc
Loi Le <huyloile@gmail.com>
2021-03-01 06:01:19 +0100

    Add @Secured(SecurityRule.IS_ANONYMOUS) to bypass the unit tests #10

--- src/main/java/com/mn/broker/MarketsController.java
+++ src/main/java/com/mn/broker/MarketsController.java
@@ -5,6 +5,8 @@ import com.mn.broker.store.InMemoryStore;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -12,6 +14,7 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.List;
 
+@Secured(SecurityRule.IS_ANONYMOUS)
 @Controller("/markets")
 public class MarketsController {
 
--- src/main/java/com/mn/broker/QuotesController.java
+++ src/main/java/com/mn/broker/QuotesController.java
@@ -7,11 +7,14 @@ import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
 import io.micronaut.http.annotation.PathVariable;
+import io.micronaut.security.annotation.Secured;
+import io.micronaut.security.rules.SecurityRule;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Secured({SecurityRule.IS_ANONYMOUS})
 @Controller("/quotes")
 public class QuotesController {
 
