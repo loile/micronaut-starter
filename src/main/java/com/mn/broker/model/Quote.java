053d1746d6ba27512216bd290c4c46c86a146d44
Loi Le <huyloile@gmail.com>
2021-03-04 00:29:35 +0100

    Flyway db migration #12

--- src/main/java/com/mn/broker/model/Quote.java
+++ src/main/java/com/mn/broker/model/Quote.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @Builder
 @AllArgsConstructor
@@ -12,4 +14,8 @@ import lombok.NoArgsConstructor;
 public class Quote {
 
     private Symbol symbol;
+    private BigDecimal bid;
+    private BigDecimal ask;
+    private BigDecimal lastPrice;
+    private BigDecimal volume;
 }
